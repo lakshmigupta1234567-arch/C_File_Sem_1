6f8056b687b282f833a7817b3fe6f02d35ff71f8
Luxmi Gupta <lakshmigupta1234567@gmail.com>
2025-12-29 01:15:38 +0530

    Add C practice programs

---
+++ 01_Practical.c
@@ -0,0 +1,7 @@
+#include <stdio.h>   // Standard I/O library
+
+int main() {
+    printf("Name: Luxmi Gupta\n");  // Display name
+    printf("Age: 16\n");            // Display age
+    return 0;                       // Program executed successfully
+}
